f34311eed61f3131ec05ef5d1dd12ef2a7fdebd4
creepydanunity <mr.creepydanx@gmail.com>
2022-11-14 13:24:35 +0300

    Ex. 1.2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
+import java.io.*;
+import java.nio.channels.NonWritableChannelException;
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try(FileInputStream in = new FileInputStream("src/input.txt"); FileOutputStream out = new FileOutputStream("output.txt")){
+            byte[] buffer = new byte[in.available()];
+            in.read(buffer, 0, buffer.length);
+            out.write(buffer, 0, buffer.length);
+
+        }
+        catch(FileNotFoundException ex) {
+            System.out.println("File not found");
+        }
+        catch(NonWritableChannelException ex) {
+            System.out.println("Non-writable output file");
+        }
+        catch(IOException ex) {
+            System.out.println(ex.toString());
+        }
     }
 }
